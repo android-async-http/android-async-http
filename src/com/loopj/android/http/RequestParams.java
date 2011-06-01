e8c1d6148f6bfe5a76e4e65a27c265c249c49d47
James Smith <james@loopj.com>
2011-05-31 21:27:05 -0700

    File uploads now work

--- src/com/loopj/android/http/RequestParams.java
+++ src/com/loopj/android/http/RequestParams.java
@@ -18,6 +18,8 @@
 
 package com.loopj.android.http;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.UnsupportedEncodingException;
 import java.util.LinkedList;
 import java.util.List;
@@ -32,7 +34,8 @@ import org.apache.http.message.BasicNameValuePair;
 public class RequestParams {
     private static String ENCODING = "UTF-8";
 
-    protected ConcurrentHashMap<String,String> urlParams;
+    protected ConcurrentHashMap<String, String> stringParams;
+    protected ConcurrentHashMap<String, FileWrapper> fileParams;
 
     public RequestParams() {
         init();
@@ -54,39 +57,125 @@ public class RequestParams {
 
     public void put(String key, String value){
         if(key != null && value != null) {
-            urlParams.put(key,value);
+            stringParams.put(key, value);
+        }
+    }
+
+    public void put(String key, ByteArrayInputStream filedata) {
+        put(key, filedata, null, null);
+    }
+
+    public void put(String key, ByteArrayInputStream filedata, String filename) {
+        put(key, filedata, filename, null);
+    }
+
+    public void put(String key, ByteArrayInputStream filedata, String filename, String contentType){
+        if(key != null && filedata != null) {
+            fileParams.put(key, new FileWrapper(filedata, filename, contentType));
         }
     }
 
     public void remove(String key){
-        urlParams.remove(key);
+        stringParams.remove(key);
+        fileParams.remove(key);
     }
 
     public String getParamString() {
+        if(!fileParams.isEmpty()) {
+            throw new RuntimeException("Uploading files is not supported with Http GET requests.");
+        }
+
         return URLEncodedUtils.format(getParamsList(), ENCODING);
     }
 
     public HttpEntity getEntity() {
         HttpEntity entity = null;
-        try {
-            entity = new UrlEncodedFormEntity(getParamsList(), ENCODING);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+
+        if(!fileParams.isEmpty()) {
+            SimpleMultipartEntity multipartEntity = new SimpleMultipartEntity();
+
+            // Add string params
+            for(ConcurrentHashMap.Entry<String, String> entry : stringParams.entrySet()) {
+                multipartEntity.addPart(entry.getKey(), entry.getValue());
+            }
+
+            // Add file params
+            for(ConcurrentHashMap.Entry<String, FileWrapper> entry : fileParams.entrySet()) {
+                FileWrapper file = entry.getValue();
+                if(file.bytes != null) {
+                    String filename = file.filename;
+                    if(filename == null) {
+                        filename = "nofilename";
+                    }
+
+                    if(file.contentType != null) {
+                        multipartEntity.addPart(entry.getKey(), filename, file.bytes, file.contentType);
+                    } else {
+                        multipartEntity.addPart(entry.getKey(), filename, file.bytes);
+                    }
+                }
+            }
+
+            entity = multipartEntity;
+        } else {
+            try {
+                entity = new UrlEncodedFormEntity(getParamsList(), ENCODING);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
         }
+
         return entity;
     }
 
     private void init(){
-        urlParams = new ConcurrentHashMap<String,String>();
+        stringParams = new ConcurrentHashMap<String, String>();
+        fileParams = new ConcurrentHashMap<String, FileWrapper>();
     }
 
     private List<BasicNameValuePair> getParamsList() {
         List<BasicNameValuePair> lparams = new LinkedList<BasicNameValuePair>();
 
-        for(ConcurrentHashMap.Entry<String, String> entry : urlParams.entrySet()) {
+        for(ConcurrentHashMap.Entry<String, String> entry : stringParams.entrySet()) {
             lparams.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
         }
 
         return lparams;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for(ConcurrentHashMap.Entry<String, String> entry : stringParams.entrySet()) {
+            if(result.length() > 0)
+                result.append("&");
+
+            result.append(entry.getKey());
+            result.append("=");
+            result.append(entry.getValue());
+        }
+
+        for(ConcurrentHashMap.Entry<String, FileWrapper> entry : fileParams.entrySet()) {
+            if(result.length() > 0)
+                result.append("&");
+
+            result.append(entry.getKey());
+            result.append("=");
+            result.append("FILE");
+        }
+
+        return result.toString();
+    }
+
+    private static class FileWrapper {
+        public ByteArrayInputStream bytes;
+        public String filename;
+        public String contentType;
+
+        public FileWrapper(ByteArrayInputStream bytes, String filename, String contentType) {
+            this.bytes = bytes;
+            this.filename = filename;
+            this.contentType = contentType;
+        }
     }
 }
