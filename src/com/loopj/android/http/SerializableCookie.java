80fde0d6d395ffb307a618d56cf40726fedf39ea
Scott Anderson <sanderson@prospectiv.com>
2011-08-23 14:46:59 -0400

    Generics, final, foreach, remove unused String/import

--- src/com/loopj/android/http/SerializableCookie.java
+++ src/com/loopj/android/http/SerializableCookie.java
@@ -34,7 +34,7 @@ import org.apache.http.impl.cookie.BasicClientCookie;
 public class SerializableCookie implements Serializable {
     private static final long serialVersionUID = 6374381828722046732L;
 
-    private transient Cookie cookie;
+    private transient final Cookie cookie;
     private transient BasicClientCookie clientCookie;
 
     public SerializableCookie(Cookie cookie) {
