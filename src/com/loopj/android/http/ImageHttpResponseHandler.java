6407f0468869b5189fc4d897c33a0fcdd6e6520a
Linden Darling <coreform@gmail.com>
2012-03-01 22:52:00 +1100

    Quick, untested implementation of a ResponseHandler for images.

---
+++ src/com/loopj/android/http/ImageHttpResponseHandler.java
@@ -0,0 +1,227 @@
+/*
+    Android Asynchronous Http Client
+    Copyright (c) 2011 James Smith <james@loopj.com>
+    http://loopj.com
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+package com.loopj.android.http;
+
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.entity.BufferedHttpEntity;
+import org.apache.http.util.EntityUtils;
+
+import android.os.Handler;
+import android.os.Message;
+import android.os.Looper;
+
+/**
+ * Used to intercept and handle the responses from requests made using 
+ * {@link AsyncHttpClient}, with automatic handling of png/jpg image data
+ * (e.g. checks Content-type, Content-length). Receives response body as
+ * byte array.
+ * <p>
+ * Additionally, you can override the {@link #onFailure(Throwable, String)},
+ * {@link #onStart()}, and {@link #onFinish()} methods as required.
+ * <p>
+ * For example:
+ * <p>
+ * <pre>
+ * AsyncHttpClient client = new AsyncHttpClient();
+ * client.get("http://www.google.com", new AsyncHttpResponseHandler() {
+ *     &#064;Override
+ *     public void onStart() {
+ *         // Initiated the request
+ *     }
+ *
+ *     &#064;Override
+ *     public void onSuccess(String response) {
+ *         // Successfully got a response
+ *     }
+ * 
+ *     &#064;Override
+ *     public void onFailure(Throwable e, String response) {
+ *         // Response failed :(
+ *     }
+ *
+ *     &#064;Override
+ *     public void onFinish() {
+ *         // Completed the request (either success or failure)
+ *     }
+ * });
+ * </pre>
+ */
+public class ImageHttpResponseHandler {
+    private static final int SUCCESS_MESSAGE = 0;
+    private static final int FAILURE_MESSAGE = 1;
+    private static final int START_MESSAGE = 2;
+    private static final int FINISH_MESSAGE = 3;
+
+    private Handler handler;
+
+    /**
+     * Creates a new AsyncHttpResponseHandler
+     */
+    public ImageHttpResponseHandler() {
+        // Set up a handler to post events back to the correct thread if possible
+        if(Looper.myLooper() != null) {
+            handler = new Handler(){
+                public void handleMessage(Message msg){
+                    ImageHttpResponseHandler.this.handleMessage(msg);
+                }
+            };
+        }
+    }
+
+
+    //
+    // Callbacks to be overridden, typically anonymously
+    //
+
+    /**
+     * Fired when the request is started, override to handle in your own code
+     */
+    public void onStart() {}
+
+    /**
+     * Fired in all cases when the request is finished, after both success and failure, override to handle in your own code
+     */
+    public void onFinish() {}
+
+    /**
+     * Fired when a request returns successfully, override to handle in your own code
+     * @param content the body of the HTTP response from the server
+     */
+    public void onSuccess(byte[] imageData) {}
+
+    /**
+     * Fired when a request fails to complete, override to handle in your own code
+     * @param error the underlying cause of the failure
+     * @deprecated use {@link #onFailure(Throwable, String)}
+     */
+    public void onFailure(Throwable error) {}
+
+    /**
+     * Fired when a request fails to complete, override to handle in your own code
+     * @param error the underlying cause of the failure
+     * @param content the response body, if any
+     */
+    public void onFailure(Throwable error, byte[] imageData) {
+        // By default, call the deprecated onFailure(Throwable) for compatibility
+        onFailure(error);
+    }
+
+
+    //
+    // Pre-processing of messages (executes in background threadpool thread)
+    //
+
+    protected void sendSuccessMessage(byte[] responseBody) {
+        sendMessage(obtainMessage(SUCCESS_MESSAGE, responseBody));
+    }
+
+    protected void sendFailureMessage(Throwable e, byte[] responseBody) {
+        sendMessage(obtainMessage(FAILURE_MESSAGE, new Object[]{e, responseBody}));
+    }
+
+    protected void sendStartMessage() {
+        sendMessage(obtainMessage(START_MESSAGE, null));
+    }
+
+    protected void sendFinishMessage() {
+        sendMessage(obtainMessage(FINISH_MESSAGE, null));
+    }
+
+
+    //
+    // Pre-processing of messages (in original calling thread, typically the UI thread)
+    //
+
+    protected void handleSuccessMessage(byte[] responseBody) {
+        onSuccess(responseBody);
+    }
+
+    protected void handleFailureMessage(Throwable e, byte[] responseBody) {
+        onFailure(e, responseBody);
+    }
+
+
+
+    // Methods which emulate android's Handler and Message methods
+    protected void handleMessage(Message msg) {
+        switch(msg.what) {
+            case SUCCESS_MESSAGE:
+                handleSuccessMessage((byte[])msg.obj);
+                break;
+            case FAILURE_MESSAGE:
+                Object[] response = (Object[])msg.obj;
+                handleFailureMessage((Throwable)response[0], (byte[])response[1]);
+                break;
+            case START_MESSAGE:
+                onStart();
+                break;
+            case FINISH_MESSAGE:
+                onFinish();
+                break;
+        }
+    }
+
+    protected void sendMessage(Message msg) {
+        if(handler != null){
+            handler.sendMessage(msg);
+        } else {
+            handleMessage(msg);
+        }
+    }
+
+    protected Message obtainMessage(int responseMessage, Object response) {
+        Message msg = null;
+        if(handler != null){
+            msg = this.handler.obtainMessage(responseMessage, response);
+        }else{
+            msg = new Message();
+            msg.what = responseMessage;
+            msg.obj = response;
+        }
+        return msg;
+    }
+
+
+    // Interface to AsyncHttpRequest
+    void sendResponseMessage(HttpResponse response) {
+        StatusLine status = response.getStatusLine();
+        byte[] responseBody = null;
+        try {
+            HttpEntity entity = null;
+            HttpEntity temp = response.getEntity();
+            if(temp != null) {
+                entity = new BufferedHttpEntity(temp);
+            }
+            responseBody = EntityUtils.toByteArray(entity);
+        } catch(IOException e) {
+            sendFailureMessage(e, null);
+        }
+
+        if(status.getStatusCode() >= 300) {
+            sendFailureMessage(new HttpResponseException(status.getStatusCode(), status.getReasonPhrase()), responseBody);
+        } else {
+            sendSuccessMessage(responseBody);
+        }
+    }
+}
