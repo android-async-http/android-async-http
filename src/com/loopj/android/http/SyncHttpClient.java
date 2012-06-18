83dfdec4a73868ae08f001439775b3089864f887
unknown <Spobo@SpoboSmall.(none)>
2012-06-18 09:55:31 +0200

    Signed-off-by: unknown <Spobo@SpoboSmall.(none)>

--- src/com/loopj/android/http/SyncHttpClient.java
+++ src/com/loopj/android/http/SyncHttpClient.java
@@ -5,9 +5,10 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.HttpContext;
 
 import android.content.Context;
+import android.os.Message;
 
 public abstract class SyncHttpClient extends AsyncHttpClient {
-
+	private int responseCode;
 	/*
 	 * as this is a synchronous request this is just a helping mechanism to pass
 	 * the result back to this method. Therefore the result object has to be a
@@ -15,6 +16,21 @@ public abstract class SyncHttpClient extends AsyncHttpClient {
 	 */
 	private String result;
 	AsyncHttpResponseHandler responseHandler = new AsyncHttpResponseHandler() {
+
+		void sendResponseMessage(org.apache.http.HttpResponse response) {
+			responseCode = response.getStatusLine().getStatusCode();
+			super.sendResponseMessage(response);
+		};
+
+		@Override
+		protected void sendMessage(Message msg) {
+			/*
+			 * Dont use the handler and send it directly to the analysis
+			 * (because its all the same thread)
+			 */
+			handleMessage(msg);
+		}
+
 		@Override
 		public void onSuccess(String content) {
 			result = content;
@@ -25,6 +41,14 @@ public abstract class SyncHttpClient extends AsyncHttpClient {
 			result = onRequestFailed(error, content);
 		}
 	};
+
+	/**
+	 * @return the response code for the last request, might be usefull
+	 *         sometimes
+	 */
+	public int getResponseCode() {
+		return responseCode;
+	}
 
 	// Private stuff
 	protected void sendRequest(DefaultHttpClient client,
@@ -40,7 +64,6 @@ public abstract class SyncHttpClient extends AsyncHttpClient {
 		 */
 		new AsyncHttpRequest(client, httpContext, uriRequest, responseHandler)
 				.run();
-
 	}
 
 	public abstract String onRequestFailed(Throwable error, String content);
@@ -60,8 +83,18 @@ public abstract class SyncHttpClient extends AsyncHttpClient {
 		return result;
 	}
 
+	public String get(String url) {
+		this.get(url, null, responseHandler);
+		return result;
+	}
+
 	public String put(String url, RequestParams params) {
 		this.put(url, params, responseHandler);
+		return result;
+	}
+
+	public String put(String url) {
+		this.put(url, null, responseHandler);
 		return result;
 	}
 
@@ -70,8 +103,19 @@ public abstract class SyncHttpClient extends AsyncHttpClient {
 		return result;
 	}
 
+	public String post(String url) {
+		this.post(url, null, responseHandler);
+		return result;
+	}
+
 	public String delete(String url, RequestParams params) {
 		this.delete(url, params, responseHandler);
 		return result;
 	}
+
+	public String delete(String url) {
+		this.delete(url, null, responseHandler);
+		return result;
+	}
+
 }
