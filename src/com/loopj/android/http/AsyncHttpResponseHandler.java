6e3c01b287c24e4a0c49c4d5aded900990cb60dd
xAnubiSx <xAnubiSx@gmail.com>
2013-08-16 23:47:45 -0300

    added javadoc

--- src/com/loopj/android/http/AsyncHttpResponseHandler.java
+++ src/com/loopj/android/http/AsyncHttpResponseHandler.java
@@ -1,5 +1,40 @@
 package com.loopj.android.http;
 
+/**
+ * Used to intercept and handle the responses from requests made using 
+ * {@link AsyncHttpClient}. The {@link #onSuccess(String)} method is 
+ * designed to be anonymously overridden with your own response handling code.
+ * <p>
+ * Additionally, you can override the {@link #onFailure(Throwable, String)},
+ * {@link #onStart()}, and {@link #onFinish()} methods as required.
+ * <p>
+ * For example:
+ * <p>
+ * <pre>
+ * AsyncHttpClient client = new AsyncHttpClient();
+ * client.get("http://www.google.com", new AsyncHttpResponseHandler() {
+ *     &#064;Override
+ *     public void onStart() {
+ *         // Initiated the request
+ *     }
+ *
+ *     &#064;Override
+ *     public void onSuccess(String response) {
+ *         // Successfully got a response
+ *     }
+ * 
+ *     &#064;Override
+ *     public void onFailure(Throwable e, String response) {
+ *         // Response failed :(
+ *     }
+ *
+ *     &#064;Override
+ *     public void onFinish() {
+ *         // Completed the request (either success or failure)
+ *     }
+ * });
+ * </pre>
+ */
 public class AsyncHttpResponseHandler extends HttpResponseHandler {
 
 }
