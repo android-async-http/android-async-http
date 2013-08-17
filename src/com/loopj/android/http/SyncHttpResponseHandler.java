bedd681003f8d9e1d7b99e2c91bd8c6194cddb81
xAnubiSx <xAnubiSx@gmail.com>
2013-08-16 23:47:11 -0300

    added class SyncHttpResponseHandler

---
+++ src/com/loopj/android/http/SyncHttpResponseHandler.java
@@ -0,0 +1,44 @@
+package com.loopj.android.http;
+
+import android.os.Message;
+
+/**
+ * Used to intercept and handle the responses from requests made using 
+ * {@link AsyncHttpClient}. The {@link #onSuccess(String)} method is 
+ * designed to be anonymously overridden with your own response handling code.
+ * <p>
+ * Additionally, you can override the {@link #onFailure(Throwable, String)},
+ * {@link #onStart()}, and {@link #onFinish()} methods as required.
+ * <p>
+ * For example:
+ * <p>
+ * <pre>
+ * AsyncHttpClient client = new AsyncHttpClient();
+ * client.get("http://www.google.com", new SyncHttpResponseHandler() {
+ *     &#064;Override
+ *     public void onStart() {
+ *         // Initiated the request
+ *     }
+ *
+ *     &#064;Override
+ *     public void onSuccess(String response) {
+ *         // Successfully got a response
+ *     }
+ * 
+ *     &#064;Override
+ *     public void onFailure(Throwable e, String response) {
+ *         // Response failed :(
+ *     }
+ *
+ *     &#064;Override
+ *     public void onFinish() {
+ *         // Completed the request (either success or failure)
+ *     }
+ * });
+ * </pre>
+ */
+public class SyncHttpResponseHandler extends HttpResponseHandler {
+	protected void sendMessage(Message msg) {
+		handleMessage(msg);
+    }
+}
