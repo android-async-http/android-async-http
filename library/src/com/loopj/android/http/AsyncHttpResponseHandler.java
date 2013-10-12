d3b52ee3367f9585da3a05142f830a3eb56fb68e
mareksebera <marek.sebera@gmail.com>
2013-10-12 21:17:23 +0200

    Fixes #294

--- library/src/com/loopj/android/http/AsyncHttpResponseHandler.java
+++ library/src/com/loopj/android/http/AsyncHttpResponseHandler.java
@@ -253,6 +253,7 @@ public class AsyncHttpResponseHandler {
             }
         } catch (IOException e) {
             sendFailureMessage(e, (String) null);
+            return;
         }
 
         if (status.getStatusCode() >= 300) {
