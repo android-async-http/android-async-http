898e18ea7c14b9e1f96deed857254712ed70287a
Peter Edwards <sweetlilmre@gmail.com>
2013-10-16 15:03:20 +0200

    Undid example change for now

--- examples/ExampleUsage.java
+++ examples/ExampleUsage.java
@@ -2,7 +2,7 @@ public class ExampleUsage {
     public static void makeRequest() {
         AsyncHttpClient client = new AsyncHttpClient();
 
-        client.get("http://www.google.com", new TextHttpResponseHandler() {
+        client.get("http://www.google.com", new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(String response) {
                 System.out.println(response);
