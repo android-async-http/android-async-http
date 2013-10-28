1cdef6301aaf6538942af619a942808d0ef802d0
mareksebera <marek.sebera@gmail.com>
2013-10-28 15:57:16 +0100

    Sample implemented File

--- sample/src/main/java/com/loopj/android/http/sample/FileSample.java
+++ sample/src/main/java/com/loopj/android/http/sample/FileSample.java
@@ -1,7 +1,85 @@
 package com.loopj.android.http.sample;
 
-import android.app.Activity;
+import android.util.Log;
 
-public class FileSample extends Activity {
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.FileAsyncHttpResponseHandler;
+import com.loopj.android.http.sample.util.FileUtil;
 
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+
+import java.io.File;
+
+public class FileSample extends SampleParentActivity {
+    private static final String LOG_TAG = "FileSample";
+
+    @Override
+    protected int getSampleTitle() {
+        return R.string.title_file_sample;
+    }
+
+    @Override
+    protected boolean isRequestBodyAllowed() {
+        return false;
+    }
+
+    @Override
+    protected boolean isRequestHeadersAllowed() {
+        return true;
+    }
+
+    @Override
+    protected String getDefaultURL() {
+        return "https://httpbin.org/robots.txt";
+    }
+
+    @Override
+    protected AsyncHttpResponseHandler getResponseHandler() {
+        try {
+            File tmpFile = File.createTempFile("temp_", "_response", getCacheDir());
+            return new FileAsyncHttpResponseHandler(tmpFile) {
+                @Override
+                public void onStart() {
+                    clearOutputs();
+                }
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, File file) {
+                    debugHeaders(LOG_TAG, headers);
+                    debugStatusCode(LOG_TAG, statusCode);
+                    debugFile(getTargetFile());
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                    debugHeaders(LOG_TAG, headers);
+                    debugStatusCode(LOG_TAG, statusCode);
+                    debugThrowable(LOG_TAG, e);
+                    debugFile(getTargetFile());
+                }
+
+                private void debugFile(File file) {
+                    if (file == null || !file.exists()) {
+                        debugResponse(LOG_TAG, "Response is null");
+                        return;
+                    }
+                    try {
+                        debugResponse(LOG_TAG, FileUtil.getStringFromFile(file));
+                    } catch (Throwable t) {
+                        Log.e(LOG_TAG, "Cannot debug file contents", t);
+                    }
+                }
+            };
+        } catch (Throwable t) {
+            Log.e("FileSample", "Cannot create temporary file", t);
+        }
+        return null;
+    }
+
+    @Override
+    protected void executeSample(AsyncHttpClient client, String URL, Header[] headers, HttpEntity entity, AsyncHttpResponseHandler responseHandler) {
+        client.get(this, URL, headers, null, responseHandler);
+    }
 }
---
+++ sample/src/main/java/com/loopj/android/http/sample/util/FileUtil.java
@@ -0,0 +1,30 @@
+package com.loopj.android.http.sample.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+// Source: http://stackoverflow.com/questions/12910503/android-read-file-as-string
+public class FileUtil {
+
+    public static String convertStreamToString(InputStream is) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        return sb.toString();
+    }
+
+    public static String getStringFromFile(File file) throws Exception {
+        FileInputStream fin = new FileInputStream(file);
+        String ret = convertStreamToString(fin);
+        //Make sure you close all streams.
+        fin.close();
+        return ret;
+    }
+
+}
