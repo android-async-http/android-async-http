020508c4ccb36c4a4bacf4832673ba590bf16e0d
mareksebera <marek.sebera@gmail.com>
2013-10-28 16:05:30 +0100

    Modified FileAsyncHttpResponseHandler with helper methods and alternative constructors

--- library/src/com/loopj/android/http/FileAsyncHttpResponseHandler.java
+++ library/src/com/loopj/android/http/FileAsyncHttpResponseHandler.java
@@ -1,5 +1,8 @@
 package com.loopj.android.http;
 
+import android.content.Context;
+import android.util.Log;
+
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 
@@ -12,10 +15,32 @@ import java.io.InputStream;
 public class FileAsyncHttpResponseHandler extends AsyncHttpResponseHandler {
 
     private File mFile;
+    private static final String LOG_TAG = "FileAsyncHttpResponseHandler";
 
     public FileAsyncHttpResponseHandler(File file) {
         super();
+        assert (file != null);
         this.mFile = file;
+    }
+
+    public FileAsyncHttpResponseHandler(Context c) {
+        super();
+        assert (c != null);
+        this.mFile = getTemporaryFile(c);
+    }
+
+    protected File getTemporaryFile(Context c) {
+        try {
+            return File.createTempFile("temp_", "_handled", c.getCacheDir());
+        } catch (Throwable t) {
+            Log.e(LOG_TAG, "Cannot create temporary file", t);
+        }
+        return null;
+    }
+
+    protected File getTargetFile() {
+        assert (mFile != null);
+        return mFile;
     }
 
     public void onSuccess(File file) {
@@ -23,6 +48,10 @@ public class FileAsyncHttpResponseHandler extends AsyncHttpResponseHandler {
 
     public void onSuccess(int statusCode, File file) {
         onSuccess(file);
+    }
+
+    public void onSuccess(int statusCode, Header[] headers, File file) {
+        onSuccess(statusCode, file);
     }
 
     public void onFailure(Throwable e, File response) {
@@ -42,38 +71,38 @@ public class FileAsyncHttpResponseHandler extends AsyncHttpResponseHandler {
 
     @Override
     public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-        onFailure(statusCode, headers, error, mFile);
+        onFailure(statusCode, headers, error, getTargetFile());
     }
 
     @Override
     public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-        onSuccess(statusCode, mFile);
+        onSuccess(statusCode, headers, getTargetFile());
     }
 
     @Override
     byte[] getResponseData(HttpEntity entity) throws IOException {
-      if (entity != null) {
-          InputStream instream = entity.getContent();
-          long contentLength = entity.getContentLength();
-          FileOutputStream buffer = new FileOutputStream(this.mFile);
-          if (instream != null) {
-              try {
-                  byte[] tmp = new byte[BUFFER_SIZE];
-                  int l, count = 0;
-                  // do not send messages if request has been cancelled
-                  while ((l = instream.read(tmp)) != -1 && !Thread.currentThread().isInterrupted()) {
-                      count += l;
-                      buffer.write(tmp, 0, l);
-                      sendProgressMessage(count, (int) contentLength);
-                  }
-              } finally {
-                  instream.close();
-                  buffer.flush();
-                  buffer.close();
-              }
-          }
-      }
-      return null;
-  }
-  
+        if (entity != null) {
+            InputStream instream = entity.getContent();
+            long contentLength = entity.getContentLength();
+            FileOutputStream buffer = new FileOutputStream(getTargetFile());
+            if (instream != null) {
+                try {
+                    byte[] tmp = new byte[BUFFER_SIZE];
+                    int l, count = 0;
+                    // do not send messages if request has been cancelled
+                    while ((l = instream.read(tmp)) != -1 && !Thread.currentThread().isInterrupted()) {
+                        count += l;
+                        buffer.write(tmp, 0, l);
+                        sendProgressMessage(count, (int) contentLength);
+                    }
+                } finally {
+                    instream.close();
+                    buffer.flush();
+                    buffer.close();
+                }
+            }
+        }
+        return null;
+    }
+
 }
