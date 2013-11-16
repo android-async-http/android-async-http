70ccbd656f531bf30ca223e6b91d42607fe413e9
Oriental Sensation <github@orientalsensation.com>
2013-11-16 17:20:13 +0100

    There's no need to escape slash character since content is already base64-encoded.

--- library/src/main/java/com/loopj/android/http/JsonStreamerEntity.java
+++ library/src/main/java/com/loopj/android/http/JsonStreamerEntity.java
@@ -288,9 +288,6 @@ class JsonStreamerEntity implements HttpEntity {
                 case '\t':
                     BUILDER.append("\\t");
                     break;
-                case '/':
-                    BUILDER.append("\\/");
-                    break;
                 default:
                     // Reference: http://www.unicode.org/versions/Unicode5.1.0/
                     if((ch >= '\u0000' && ch <= '\u001F') || (ch >= '\u007F' && ch <= '\u009F') || (ch >= '\u2000' && ch <= '\u20FF')) {
