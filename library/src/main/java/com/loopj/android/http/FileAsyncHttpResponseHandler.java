030d0bcfc419429f8c3019824bff297d3e209f84
mareksebera <marek.sebera@gmail.com>
2014-03-20 20:00:59 +0100

    Added RangeFileAsyncHttpResponseHandler, #423

--- library/src/main/java/com/loopj/android/http/FileAsyncHttpResponseHandler.java
+++ library/src/main/java/com/loopj/android/http/FileAsyncHttpResponseHandler.java
@@ -14,7 +14,7 @@ import java.io.InputStream;
 
 public abstract class FileAsyncHttpResponseHandler extends AsyncHttpResponseHandler {
 
-    private final File mFile;
+    protected final File mFile;
     private static final String LOG_TAG = "FileAsyncHttpResponseHandler";
 
     /**
