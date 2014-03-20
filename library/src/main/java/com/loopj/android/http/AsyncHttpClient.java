4f40cac56cc3a9564d2ba26d1d0696f87c14ca49
mareksebera <marek.sebera@gmail.com>
2014-03-20 20:05:27 +0100

    Added cancelAllRequests method, #423

--- library/src/main/java/com/loopj/android/http/AsyncHttpClient.java
+++ library/src/main/java/com/loopj/android/http/AsyncHttpClient.java
@@ -505,6 +505,26 @@ public class AsyncHttpClient {
         }
     }
 
+    /**
+     * Cancels all pending (or potentially active) requests. <p>&nbsp;</p> <b>Note:</b> This will
+     * only affect requests which were created with a non-null android Context. This method is
+     * intended to be used in the onDestroy method of your android activities to destroy all
+     * requests which are no longer required.
+     *
+     * @param mayInterruptIfRunning specifies if active requests should be cancelled along with
+     *                              pending requests.
+     */
+    public void cancelAllRequests(boolean mayInterruptIfRunning) {
+        for (List<RequestHandle> requestList : requestMap.values()) {
+            if (requestList != null) {
+                for (RequestHandle requestHandle : requestList) {
+                    requestHandle.cancel(mayInterruptIfRunning);
+                }
+            }
+        }
+        requestMap.clear();
+    }
+
     // [+] HTTP HEAD
 
     /**
