8800a7dc283553d803106e9e6221678eac90186a
mareksebera <marek.sebera@gmail.com>
2014-03-29 17:28:50 +0100

    Added logging to RequestHandle cancelling

--- sample/src/main/java/com/loopj/android/http/sample/CancelRequestHandleSample.java
+++ sample/src/main/java/com/loopj/android/http/sample/CancelRequestHandleSample.java
@@ -1,8 +1,12 @@
 package com.loopj.android.http.sample;
 
+import android.util.Log;
+
 import com.loopj.android.http.RequestHandle;
 
 public class CancelRequestHandleSample extends ThreadingTimeoutSample {
+
+    private static final String LOG_TAG = "ThreadingTimeoutSample";
 
     @Override
     public int getSampleTitle() {
@@ -11,10 +15,16 @@ public class CancelRequestHandleSample extends ThreadingTimeoutSample {
 
     @Override
     public void onCancelButtonPressed() {
+        Log.d(LOG_TAG, String.format("Number of handles found: %d", getRequestHandles().size()));
+        int counter = 0;
         for (RequestHandle handle : getRequestHandles()) {
             if (!handle.isCancelled() && !handle.isFinished()) {
-                handle.cancel(true);
+                Log.d(LOG_TAG, String.format("Cancelling handle %d", counter));
+                Log.d(LOG_TAG, String.format("Handle %d cancel", counter) + (handle.cancel(true) ? " succeeded" : " failed"));
+            } else {
+                Log.d(LOG_TAG, String.format("Handle %d already non-cancellable", counter));
             }
+            counter++;
         }
     }
 }
